c6b3d30e2038db6913c15426d23d524545d400f6
Anna Pawl <annampawl@gmail.com>
2019-08-29 14:13:38 -0700

    showing product

--- src/App.js
+++ src/App.js
@@ -3,19 +3,42 @@ import inventory, { categories } from './inventory'
 import './App.css';
 
 class App extends Component {
-  
+
   render() {
+
+    const cats = categories.map((item, index) => {
+      return (
+        <button>
+          {item}
+        </button>
+      )
+    })
+
+    const products = inventory.map(({ name, id, category, description, price }, index) => {
+      return (
+        <div>
+          <h1>{name}</h1>
+          <p>{description}</p>
+          <p>{price}</p>
+        </div>
+      )
+    })
+
     return (
       <div className="App">
         <h1>Show products here</h1>
 
         <ul>
           {/* List product categories here */}
+          {cats}
         </ul>
 
         <ul>
-          {/* Products listed here */}
+          {/* List product categories here */}
+          {products}
         </ul>
+
+
 
       </div>
     );
